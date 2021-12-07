21e7ce7b807b5ce50b68a9c6c8b7b7629b97e8d5
Kerberos <67758795+kerberos1999@users.noreply.github.com>
2021-12-07 02:11:37 +0100

    tanks array
    [+] changed all functions to work with tank struct

---
+++ 1DAE10_Tanks_FroeseDavid_AzarafrozNick/Tanks/Game.cpp
@@ -0,0 +1,283 @@
+#include "pch.h"
+#include "Game.h"
+#include <iostream>
+
+//Basic game functions
+#pragma region gameFunctions											
+void Start()
+{
+	//LoadTextures();
+	InitTanks();
+}
+
+void Draw()
+{
+	ClearBackground();
+
+	//DrawProjectiles();
+	DrawTanks();
+
+	//-------------------- UI -----------------------------
+
+	DrawHealthBars();
+}
+
+void Update(float elapsedSec)
+{
+	// physics
+	//UpdateProjectiles(elapsedSec);
+
+	UpdateTanks(elapsedSec);
+}
+
+void End()
+{
+	//DeleteTextures();
+	DeleteTanks();
+}
+#pragma endregion gameFunctions
+
+//Keyboard and mouse input handling
+#pragma region inputHandling											
+void OnKeyDownEvent(SDL_Keycode key)
+{
+
+}
+
+void OnKeyUpEvent(SDL_Keycode key)
+{
+	switch (key)
+	{
+	case SDLK_SPACE:
+		/*if (g_IsProjectileActive1 == false)
+		{
+			g_IsProjectileActive1 = true;
+			g_ProjectilePos1 = g_TankPos1;
+			g_ProjectileAngle = g_TankAngle1;
+		}*/
+		break;
+	case SDLK_1:
+		--g_Tanks[0].currentHP;
+		break;
+	case SDLK_2:
+		++g_Tanks[0].currentHP;
+		break;
+	case SDLK_3:
+		--g_Tanks[1].currentHP;
+		break;
+	case SDLK_4:
+		++g_Tanks[1].currentHP;
+		break;
+	}
+}
+
+void OnMouseMotionEvent(const SDL_MouseMotionEvent& e)
+{
+	//std::cout << "  [" << e.x << ", " << e.y << "]\n";
+	//Point2f mousePos{ float( e.x ), float( g_WindowHeight - e.y ) };
+}
+
+void OnMouseDownEvent(const SDL_MouseButtonEvent& e)
+{
+
+}
+
+void OnMouseUpEvent(const SDL_MouseButtonEvent& e)
+{
+	////std::cout << "  [" << e.x << ", " << e.y << "]\n";
+	//switch (e.button)
+	//{
+	//case SDL_BUTTON_LEFT:
+	//{
+	//	//std::cout << "Left mouse button released\n";
+	//	//Point2f mousePos{ float( e.x ), float( g_WindowHeight - e.y ) };
+	//	break;
+	//}
+	//case SDL_BUTTON_RIGHT:
+	//	//std::cout << "Right mouse button released\n";
+	//	break;
+	//case SDL_BUTTON_MIDDLE:
+	//	//std::cout << "Middle mouse button released\n";
+	//	break;
+	//}
+}
+#pragma endregion inputHandling
+
+#pragma region ownDefinitions
+// Define your own functions here
+
+void InitTanks()
+{
+	for (int i{}; i < g_PlayerCount; i++)
+	{
+		// textures
+		if (!TextureFromFile("Resources/healthbar_" + std::to_string(i) + ".png", g_HealthBarTextures[i]))
+			std::cout << "Resources/healthbar_" + std::to_string(i) + ".png " << "could not be loaded!" << '\n';
+
+		if (!TextureFromFile("Resources/healthbar_filling_" + std::to_string(i) + ".png", g_HealthBarFillingTextures[i]))
+			std::cout << "Resources/healthbar_filling_" + std::to_string(i) + ".png " << "could not be loaded!" << '\n';
+
+		if (!TextureFromFile("Resources/tank_" + std::to_string(i) + ".png", g_Tanks[i].texture))
+			std::cout << "Resources/tank_" + std::to_string(i) + ".png " << "could not be loaded!" << '\n';
+	
+		// attributes
+		g_Tanks[i].position = g_TankStartPositions[i];
+		g_Tanks[i].angle = 0.0f;
+		g_Tanks[i].currentHP = g_TankHP;
+		g_Tanks[i].maxHP = g_TankHP;
+		g_Tanks[i].speed = g_TankSpeed;
+		g_Tanks[i].turnSpeed = g_TankTurnSpeed;
+	}
+
+	// single textures
+	if (!TextureFromFile("Resources/healthbar_background.png", g_HealthBarBackgroundTexture))
+		std::cout << "Resources/healthbar_background.png " << "could not be loaded!" << '\n';
+
+	if (!TextureFromFile("Resources/projectile_0.png", g_ProjectileStandardTexture))
+		std::cout << "Resources/projectile_0.png " << "could not be loaded!" << '\n';
+}
+
+void DeleteTanks()
+{
+	for (int i{}; i < g_PlayerCount; i++)
+	{
+		DeleteTexture(g_HealthBarTextures[i]);
+		DeleteTexture(g_HealthBarFillingTextures[i]);
+		DeleteTexture(g_Tanks[i].texture);
+	}
+	DeleteTexture(g_HealthBarBackgroundTexture);
+	DeleteTexture(g_ProjectileStandardTexture);
+}
+
+void TurnTank(Tank& tank, float angle)
+{
+	tank.angle += angle;
+
+	// angle should not be greater than 2 * g_Pi
+	while (tank.angle > 2 * g_Pi)
+	{
+		tank.angle -= 2 * g_Pi;
+	}
+	while (tank.angle < 0)
+	{
+		tank.angle += 2 * g_Pi;
+	}
+}
+
+void UpdateTanks(float elapsedSec)
+{
+	// input for all tanks
+	const Uint8* pStates = SDL_GetKeyboardState(nullptr);
+	for (int i{}; i < g_PlayerCount; i++)
+	{
+		// turning
+		if (pStates[g_TankControls[i].rightKey])
+		{
+			TurnTank(g_Tanks[i], -g_Tanks[i].turnSpeed * elapsedSec);
+		}
+		else if (pStates[g_TankControls[i].leftKey])
+		{
+			TurnTank(g_Tanks[i], g_Tanks[i].turnSpeed * elapsedSec);
+		}
+
+		// moving
+		if (pStates[g_TankControls[i].upKey])
+		{
+			g_Tanks[i].position.x += cosf(g_Tanks[i].angle) * g_Tanks[i].speed * elapsedSec;
+			g_Tanks[i].position.y += sinf(g_Tanks[i].angle) * g_Tanks[i].speed * elapsedSec;
+		}
+		else if (pStates[g_TankControls[i].downKey])
+		{
+			g_Tanks[i].position.x -= cosf(g_Tanks[i].angle) * g_Tanks[i].speed * elapsedSec;
+			g_Tanks[i].position.y -= sinf(g_Tanks[i].angle) * g_Tanks[i].speed * elapsedSec;
+		}
+	}
+
+	// TODO: Collision detection
+
+}
+
+//void UpdateProjectile1(float elapsedSec)
+//{
+//	g_ProjectilePos1.x += cosf(g_ProjectileAngle) * g_ProjectileSpeed1 * elapsedSec;
+//	g_ProjectilePos1.y += sinf(g_ProjectileAngle) * g_ProjectileSpeed1 * elapsedSec;
+//
+//	if (g_ProjectilePos1.x >= g_WindowWidth || g_ProjectilePos1.y >= g_WindowHeight || g_ProjectilePos1.x <= 0 || g_ProjectilePos1.y <= 0)
+//	{
+//		g_IsProjectileActive1 = false;
+//	}
+//}
+
+void DrawTanks()
+{
+	// draw all tanks
+	for (int i{}; i < g_PlayerCount; i++)
+	{
+		Rectf destinationRect{};
+		destinationRect.left = g_Tanks[i].position.x - (g_Tanks[i].texture.width / 2) * g_Scaling;
+		destinationRect.bottom = g_Tanks[i].position.y - (g_Tanks[i].texture.height / 2) * g_Scaling;
+		destinationRect.width = g_Tanks[i].texture.width * g_Scaling;
+		destinationRect.height = g_Tanks[i].texture.height * g_Scaling;
+		DrawTexture(g_Tanks[i].texture, destinationRect, g_Tanks[i].angle);
+	}
+}
+
+//void DrawProjectile() 
+//{
+//	Rectf destinationProjectile{};
+//	const float offset{ 10.0f };
+//	destinationProjectile.left = (g_ProjectilePos1.x + cosf(g_TankAngle1) * offset) - (g_ProjectileTexStandard.width / 2) * g_Scaling;
+//	destinationProjectile.bottom = (g_ProjectilePos1.y + sinf(g_TankAngle1) * offset) - (g_ProjectileTexStandard.height / 2) * g_Scaling;
+//	destinationProjectile.width = g_ProjectileTexStandard.width * g_Scaling;
+//	destinationProjectile.height = g_ProjectileTexStandard.height * g_Scaling;
+//	DrawTexture(g_ProjectileTexStandard, destinationProjectile, g_ProjectileAngle);
+//}
+
+void DrawHealthBars() 
+{
+	for (int i{}; i < g_PlayerCount; i++)
+	{
+		// border
+		Rectf destinationHealth{};
+
+		destinationHealth.left = g_HealthbarPositions[i].x;
+		destinationHealth.bottom = g_HealthbarPositions[i].y;
+		destinationHealth.width = g_HealthBarTextures[i].width * g_Scaling;
+		destinationHealth.height = g_HealthBarTextures[i].height * g_Scaling;
+		DrawTexture(g_HealthBarTextures[i], destinationHealth);
+
+		// background
+		destinationHealth.width = g_HealthBarBackgroundTexture.width * g_Scaling;
+		destinationHealth.height = g_HealthBarBackgroundTexture.height * g_Scaling;
+		DrawTexture(g_HealthBarBackgroundTexture, destinationHealth);
+		if (g_Tanks[i].currentHP > 0)
+		{
+			// filling
+			float hpScale = g_Tanks[i].currentHP / g_Tanks[i].maxHP,
+				xOffset = 16.0f * g_Scaling, yOffset = 3.0f * g_Scaling;
+			destinationHealth.left = g_HealthbarPositions[i].x + xOffset;
+			destinationHealth.bottom = g_HealthbarPositions[i].y + yOffset;
+			destinationHealth.width = g_HealthBarFillingTextures[i].width * g_Scaling * hpScale;
+			destinationHealth.height = g_HealthBarFillingTextures[i].height * g_Scaling;
+			DrawTexture(g_HealthBarFillingTextures[i], destinationHealth);
+		}
+		else 
+		{
+			g_Tanks[i].currentHP = 0;
+		}
+	}
+}
+//
+//void DrawHealthBar2()
+//{
+//	DrawTexture(g_HealthBarTex2, g_HealthBarPos2);
+//	DrawTexture(g_HealthBarBackground, g_HealthBarBackgroundPos2);
+//	Rectf destinationHealth2{};
+//	float hp2 = g_TankCurrentHP2 / g_TankMaxHP2;
+//	destinationHealth2.left = g_HealthBarFillingPos2.x;
+//	destinationHealth2.bottom = g_HealthBarFillingPos2.y;
+//	destinationHealth2.width = g_HealthBarFillingTex2.width * hp2;
+//	destinationHealth2.height = g_HealthBarFillingTex2.height;
+//	DrawTexture(g_HealthBarFillingTex2, destinationHealth2);
+//}
+#pragma endregion ownDefinitions
---
+++ 1DAE10_Tanks_FroeseDavid_AzarafrozNick/Tanks/Game.h
@@ -0,0 +1,132 @@
+#pragma once
+using namespace utils;
+#pragma region gameInformation
+// Set your name and group in the title here
+std::string g_WindowTitle{ "Tanks - Froese_Azarafroz, David_Nick - 1DAE11/10" };
+
+// Change the window dimensions here
+float g_WindowWidth{ 960 };
+float g_WindowHeight{ 720 };
+#pragma endregion gameInformation
+
+
+
+#pragma region ownDeclarations
+// enums:
+enum class ProjectileState
+{
+	normal,
+	bouncing
+};
+enum class TileState
+{
+	empty,
+	unbreakableWall,
+	breakableWall,
+	woodenBox
+};
+
+// structs:
+struct Tank
+{
+	Point2f position;
+	utils::Texture texture;
+	float angle;
+	float speed;
+	float turnSpeed;
+	float currentHP;
+	float maxHP;
+};
+struct TankControls
+{
+	SDL_Scancode leftKey;
+	SDL_Scancode rightKey;
+	SDL_Scancode upKey;
+	SDL_Scancode downKey;
+
+	SDL_Keycode fireKey;
+};
+struct Projectile
+{
+	Point2f position;
+	utils::Texture texture;
+	ProjectileState state;
+	float speed;
+	float angle;
+};
+
+// const globals:
+const float g_Scaling{ 2.0f };           // scaling of all tiles, sprites, etc.
+const float g_CellSize{ 16.0f };         
+const int g_PlayerCount{ 1 };            // how many players? (up to 4)
+const float g_TankSpeed{ 100.0f };
+const float g_TankTurnSpeed{ g_Pi / 2 };
+const float g_TankHP{ 10.0f };
+
+const TankControls g_TankControls[]  // define controls here
+{
+	{SDL_SCANCODE_A, SDL_SCANCODE_D, SDL_SCANCODE_W, SDL_SCANCODE_S, SDLK_SPACE},
+	{SDL_SCANCODE_LEFT, SDL_SCANCODE_RIGHT, SDL_SCANCODE_UP, SDL_SCANCODE_DOWN, SDLK_RETURN},
+	{}
+};
+
+const Point2f g_TankStartPositions[] // define start positions here
+{
+	Point2f{ 64, 64 },
+	Point2f{ g_WindowWidth - 64, g_WindowHeight - 64 },
+	Point2f{ 64, g_WindowHeight - 64 },
+	Point2f{ g_WindowWidth - 64, 64}
+};
+
+const Point2f g_HealthbarPositions[] // define health bar positions here
+{
+	Point2f{ 20, g_WindowHeight - 40 },
+	Point2f{ g_WindowWidth - 174, g_WindowHeight - 40 },
+	Point2f{ 20, 20 },
+	Point2f{ g_WindowWidth - 174, 20 },
+};
+//const int g_MaxProjectiles{ 3 }; // max projectile count per player
+
+// globals
+Tank g_Tanks[g_PlayerCount]{};
+
+Projectile g_Projectiles[g_PlayerCount]{};    
+
+Texture g_ProjectileStandardTexture{};
+
+Texture g_HealthBarBackgroundTexture{};
+Texture g_HealthBarTextures[g_PlayerCount]{};
+Texture g_HealthBarFillingTextures[g_PlayerCount]{};
+
+//----------------------------------------------------------------------------------
+
+// Declare your own functions here
+void InitTanks();
+void DeleteTanks();
+
+void UpdateTanks(float elapsedSec);
+
+void TurnTank(Tank& tank, float angle);
+
+//void UpdateProjectiles(float elapsedSec);
+
+void DrawTanks();
+//void DrawProjectiles();
+void DrawHealthBars();
+
+#pragma endregion ownDeclarations
+
+#pragma region gameFunctions											
+void Start();
+void Draw();
+void Update(float elapsedSec);
+void End();
+#pragma endregion gameFunctions
+
+#pragma region inputHandling											
+void OnKeyDownEvent(SDL_Keycode key);
+void OnKeyUpEvent(SDL_Keycode key);
+void OnMouseMotionEvent(const SDL_MouseMotionEvent& e);
+void OnMouseDownEvent(const SDL_MouseButtonEvent& e);
+void OnMouseUpEvent(const SDL_MouseButtonEvent& e);
+#pragma endregion inputHandling
